1ff68fe3cba1b964a8e2690608653545a83322b9
kegrimes <kegrimes02@gmail.com>
2025-03-11 16:29:24 -0400

    Create UpdateUser.php

---
+++ api/UpdateUser.php
@@ -0,0 +1,62 @@
+<?php
+    $inData = getRequestInfo();
+
+    $firstName = $inData["First"];
+    $lastName = $inData["Last"];
+    $password = $inData["Password"];
+    $uid = $inData["UID"];
+    $universityName = $inData["University_name"]
+
+    $conn = new mysqli("localhost", "campusbuzz", "campus4Buzz", "CampusBuzz"); 	
+    if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $ret = $conn->prepare("SELECT First FROM Users WHERE UID=?");
+        $ret->bind_param("s", $uid);
+        $ret->execute();
+        $ret->store_result();
+
+        if( $ret->num_rows > 0){
+            $stmt = $conn->prepare("UPDATE Users SET FirstName=?, LastName=?, Password=?, UniversityName=? WHERE UID=?");
+    		$stmt->bind_param("sssss", $firstName, $lastName, $password, $universityName, $uid);
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+            returnWithError("");
+        }
+        else{
+            $ret->close();
+			$conn->close();
+			returnWithError("No User found with this UID");
+        }
+
+	}
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+    function returnWithInfo( $firstName, $lastName, $password, $universityName, $uid )
+	{
+		$retValue = '{"uid":' . $uid . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","password":"' . $password . '", "university":"' . $universityName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithError( $err )
+	{
+        $retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+?>
