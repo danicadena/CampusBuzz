b8a406a3ec3168e59e92c8fe5f258662b74e283f
kegrimes <kegrimes02@gmail.com>
2025-03-25 14:45:17 -0400

    Create UpdateUniversity.php

---
+++ api/UpdateUniversity.php
@@ -0,0 +1,59 @@
+<?php
+    $inData = getRequestInfo();
+
+    $studentCount = $inData["Student_num"];
+    $profilePic = isset($inData["Profile_pic"]) ? $inData["Profile_pic"] : null;
+
+    $conn = new mysqli("localhost", "campusbuzz", "campus4Buzz", "CampusBuzz"); 	
+    if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+        $ret = $conn->prepare("SELECT Uni_ID FROM University WHERE Uni_ID=?");
+        $ret->bind_param("s", $uniID);
+        $ret->execute();
+        $ret->store_result();
+
+        if( $ret->num_rows > 0){
+            $stmt = $conn->prepare("UPDATE University Student_num=?, Profile_pic=? WHERE Uni_ID=?");
+    		$stmt->bind_param("sss", $studentCount, $profilePic, $uniID);
+            $stmt->execute();
+            $stmt->close();
+            $conn->close();
+            returnWithError("");
+        }
+        else{
+            $ret->close();
+			$conn->close();
+			returnWithError("No University found with this id!");
+        }
+
+	}
+
+    function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+    function returnWithInfo( $uniID, $studentCount, $profilePic )
+	{
+		$retValue = '{"Uni_ID":' . $uniID . ',"Student_num":"' . $studentCount . '","Profile_pic":"' . $profilePic . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithError( $err )
+	{
+        $retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+
+?>
