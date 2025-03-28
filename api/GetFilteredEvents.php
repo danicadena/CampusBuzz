9f2b6d17f58e40b2978471a64579138f317e6a0e
kegrimes <kegrimes02@gmail.com>
2025-03-28 14:24:29 -0400

    Update GetFilteredEvents.php

--- api/GetFilteredEvents.php
+++ api/GetFilteredEvents.php
@@ -1,14 +1,6 @@
 <?php
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
-
 	$inData = getRequestInfo();
-
-    file_put_contents("debug.txt", json_encode($inData));
-
 
 	// user that is searching
 	$uid = $inData["UID"];
