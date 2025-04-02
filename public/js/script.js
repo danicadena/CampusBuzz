d7ae5e0f3bae21c4b32505fcd6d868f988d4d031
Daniela Cadena <118313592+danicadena@users.noreply.github.com>
2025-04-02 14:16:40 -0400

    modified landing page + testing register(?)

--- public/js/script.js
+++ public/js/script.js
@@ -47,20 +47,105 @@ async function doLogin(){
 
 }
 
-
 async function doRegister(){
-    let email = document.getElementById("emailInp").value();
-    let firstName = document.getElementById("firstNameInp").value();
-    let lastName = document.getElementById("lastNameInp").value();
-    let userType = document.getElementById("userType").value();
-    let university = document.getElementById("University").value();
+    //get the user input from regsiter page 
+    let email = document.getElementById("emailInp").value;
+    let firstName = document.getElementById("firstNameInp").value;
+    let lastName = document.getElementById("lastNameInp").value;
+    let userType = document.getElementById("userSelect").value; //check since dropdown:
+    let username = document.getElementById("userInp").value;
+    let password = document.getElementById("passInp").value;
+    let university = document.getElementById("userSelectSpacing").value; //check since dropdown:
+
+    document.getElementById("registerRes").innerHTML= "";
+
 
     let registerInfo = {
         Email: email,
         First : firstName,
         Last : lastName,
+        Username: username,
+        Password: password,
         User_Type : userType,
         University_name : university
     };
 
+    console.log("info being sent to backend: " + registerInfo);
+    let url = urlBase + 'Register.'+ extension;
+    console.log("url: " , url);
+
+    try{
+        const response = await fetch (url, {
+            method: 'POST', 
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(registerInfo),
+            mode : 'no-cors'
+        });
+
+        const data = await response.json();
+        console.log("data recieved: ", data);
+        
+        if (data.error && data.error !== ""){
+            //if error registering make all the fields blank
+            document.getElementById("emailInp") = "";
+            document.getElementById("firstNameInp") = "";
+            document.getElementById("lastNameInp") = "";
+            document.getElementById("userSelect") = ""; 
+            document.getElementById("userInp") = "";
+            document.getElementById("passInp") = "";
+            document.getElementById("userSelectSpacing") = ""; 
+        }
+        else{
+            //otherwise automatically log in a user 
+            window.location.href = "dashboard.html";
+        }
+
+    }catch(error){
+        document.getElementById("registerRes").innerHTML= "failed register";
+    }
+
 }
+
+//for the university dropdown we will connect to uni locations from db
+async function fetchUniversities(){
+    try{
+        let url = urlBase + 'GetLocations.'+extension;
+        console.log("url: " + url);
+        const response = await fetch(url,{
+            method: 'GET',
+            headers: {
+                'Accept' : 'application/json'
+            }, 
+            mode: 'no-cors'
+        });
+
+        const data = await response.json();
+        const uniSelect = document.getElementById('userSelectSpacing');
+
+        //a default option before user inputs something
+        universitySelect.innerHTML='';
+        const defaultOption = document.createElement('option');
+        defaultOption.textContent= 'Select University';
+        defaultOption.value = '';
+        universitySelect.appendChild(defaultOption);
+
+        if (data.results && Array.isArray(data.results)){
+            data.results.forEach(university => {
+                const option = document.getElementById('option');
+                option.textContent = university;
+                option.value = university;
+                universitySelect.appendChild(option);
+
+            })
+        } else{
+            console.log("no unis found in the response");
+        }
+
+    }catch(error){
+        console.log("error fetching unis");
+    }
+}
+
+window.onload = fetchUniversities;
