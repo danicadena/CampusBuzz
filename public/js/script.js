90c4e4644a4b48103811d04a6b226e1f59118611
Daniela Cadena <118313592+danicadena@users.noreply.github.com>
2025-04-05 12:08:46 -0400

    fix to local storage

--- public/js/script.js
+++ public/js/script.js
@@ -37,9 +37,9 @@ async function doLogin(){
             document.getElementById("usernameInp") = "";
             document.getElementById("passwordInp") = "";
         } else{
-            localStorage.setItem("UID", response.UID);
-            localStorage.setItem("first_name", response.firstName);
-            localStorage.setItem("user_type", response.user_type);
+            localStorage.setItem("UID", data.UID);
+            localStorage.setItem("first_name", data.firstName);
+            localStorage.setItem("user_type", data.user_type);
             window.location.href = "dashboard.html";
         }
 
