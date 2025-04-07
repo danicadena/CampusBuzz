cd675f1069c2cb934623c47e38bd73b974d725b2
kegrimes <kegrimes02@gmail.com>
2025-04-07 16:59:50 -0400

    update

--- public/js/script.js
+++ public/js/script.js
@@ -1267,11 +1267,10 @@ async function getSuperEvents(){
 
                     eventDiv.innerHTML = `
                         <div class="eventCardClass">
-                            <div class="cardTitle">${event.Event_name}}</div>
-                            <div class="cardType">${event.Event_type}}</div>
+                            <div class="cardTitle">${event.Event_name}</div>
                             <div class="cardDescription">${event.Description}</div>
                             <div class="cardDate">${event.Date} at ${event.Event_time}</div>
-                            <div class="cardTime">${event.Event_time}</div>
+                            <div class="cardType">${event.Event_type}</div>
                         </div>
                     `;
 
