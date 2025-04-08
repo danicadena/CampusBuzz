dd143c67866452d944892a55a612c2eac003f792
kegrimes <kegrimes02@gmail.com>
2025-04-08 13:48:26 -0400

    Update GetSuperEvents.php

--- api/GetSuperEvents.php
+++ api/GetSuperEvents.php
@@ -1,80 +1,93 @@
 <?php
 
-	$inData = getRequestInfo();
-	$uniID = $inData["Uni_ID"];
+$inData = getRequestInfo();
+$uniID = $inData["Uni_ID"];
 
-	$conn = new mysqli("localhost", "campusbuzz", "campus4Buzz", "CampusBuzz");
-	if ($conn->connect_error) 
-	{
-		returnWithError($conn->connect_error);
-	} 
-	else
-	{
-        $events = [];
+$conn = new mysqli("localhost", "campusbuzz", "campus4Buzz", "CampusBuzz");
+if ($conn->connect_error) 
+{
+    returnWithError($conn->connect_error);
+} 
+else
+{
+    $events = [];
 
-        // Get all public events
-        $getPublic = $conn->prepare("
-            SELECT E.Events_ID, E.Event_type, E.Event_name, E.Date, E.Event_time, E.Description, E.Approval_Status, L.Lname
-            FROM Events_At E
-            JOIN Public_Events_Creates P ON E.Events_ID = P.Events_ID
-            JOIN Locations L ON E.LocID = L.LocID
-			WHERE E.LocID = ?
-        ");
-		$getPublic->bind_param("i", $uniID);
-        $getPublic->execute();
-        $publicResults = $getPublic->get_result();
+    // get LocID of university
+    $getLocID = $conn->prepare("SELECT LocID FROM University WHERE Uni_ID = ?");
+    $getLocID->bind_param("i", $uniID);
+    $getLocID->execute();
+    $getLocID->bind_result($locID);
+    $getLocID->fetch();
+    $getLocID->close();
 
-        while ($row = $publicResults->fetch_assoc()) {
-            $events[] = $row;
-        }
-        $getPublic->close();
+    if (!$locID) {
+        returnWithError("No location found for university.");
+        exit;
+    }
 
-        // Get private events for a specific university
-        $getPrivate = $conn->prepare("
-            SELECT E.Events_ID, E.Event_type, E.Event_name, E.Date, E.Event_time, E.Description, E.Approval_Status, L.Lname
-            FROM Events_At E
-            JOIN Private_Events_Creates P ON E.Events_ID = P.Events_ID
-            JOIN Locations L ON E.LocID = L.LocID
-            WHERE E.LocID = ?
-        ");
-        $getPrivate->bind_param("i", $uniID);
-        $getPrivate->execute();
-        $privateResults = $getPrivate->get_result();
+    // get Public Events for this LocID
+    $getPublic = $conn->prepare("
+        SELECT E.Events_ID, E.Event_type, E.Event_name, E.Date, E.Event_time, E.Description, E.Approval_Status, L.Lname
+        FROM Events_At E
+        JOIN Public_Events_Creates P ON E.Events_ID = P.Events_ID
+        JOIN Locations L ON E.LocID = L.LocID
+        WHERE E.LocID = ?
+    ");
+    $getPublic->bind_param("i", $locID);
+    $getPublic->execute();
+    $publicResults = $getPublic->get_result();
+    while ($row = $publicResults->fetch_assoc()) {
+        $events[] = $row;
+    }
+    $getPublic->close();
 
-        while ($row = $privateResults->fetch_assoc()) {
-            $events[] = $row;
-        }
-        $getPrivate->close();
+    // get Private Events for this LocID
+    $getPrivate = $conn->prepare("
+        SELECT E.Events_ID, E.Event_type, E.Event_name, E.Date, E.Event_time, E.Description, E.Approval_Status, L.Lname
+        FROM Events_At E
+        JOIN Private_Events_Creates P ON E.Events_ID = P.Events_ID
+        JOIN Locations L ON E.LocID = L.LocID
+        WHERE E.LocID = ?
+    ");
+    $getPrivate->bind_param("i", $locID);
+    $getPrivate->execute();
+    $privateResults = $getPrivate->get_result();
+    while ($row = $privateResults->fetch_assoc()) {
+        $events[] = $row;
+    }
+    $getPrivate->close();
 
-		$conn->close();
-        returnWithInfo($events);
-	}
+    $conn->close();
+    returnWithInfo($events);
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson($obj)
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithInfo($results)
-	{
-		$retValue = json_encode([
-			"results" => $results,
-			"error" => ""
-		]);
+function sendResultInfoAsJson($obj)
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
 
-		sendResultInfoAsJson($retValue);
-	}
+function returnWithInfo($results)
+{
+    $retValue = json_encode([
+        "results" => $results,
+        "error" => ""
+    ]);
 
-	function returnWithError($err)
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson($retValue);
-	}
+    sendResultInfoAsJson($retValue);
+}
+
+function returnWithError($err)
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson($retValue);
+}
+?>
+
 
 ?>
